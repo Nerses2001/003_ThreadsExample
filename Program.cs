de9548948a6853931121dcf95888e32ac59b9fc3
Nerses Hepoyan <nerseshepoyan22@gmail.com>
2023-09-30 08:46:13 -0800

    Thread Join

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using _003_ThreadsExample;
+
+Console.WriteLine("Primary thread {0}", Thread.CurrentThread.GetHashCode());
+
+ThreadClass.WriteChar('1', 80, ConsoleColor.Green);
+
+var thread = new Thread(ThreadClass.Method2);
+
+thread.Start();
+thread.Join();
+
+ThreadClass.WriteChar('1', 80, ConsoleColor.Green);
+
+Console.WriteLine("Primary finished");
---
+++ ThreadClass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _003_ThreadsExample
+{
+    internal class ThreadClass
+    {
+
+        public static void WriteChar(char ch, int count, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            for (int i = 0; i < count; i++)
+            {
+                Thread.Sleep(20);
+                Console.Write(ch);
+            }
+        }
+
+        public static void Method2()
+        {
+            Console.WriteLine("Second Thread {0}", Thread.CurrentThread.GetHashCode());
+            WriteChar('2', 80, ConsoleColor.Blue);
+
+            var thread = new Thread(Method3);
+            thread.Start();
+            thread.Join();
+
+            WriteChar('2', 80, ConsoleColor.Blue);
+            Console.WriteLine("Second thread finished");
+        }
+
+        private static void Method3()
+        {
+            Console.WriteLine("Third Thread {0}", Thread.CurrentThread.GetHashCode());
+            WriteChar('3', 100, ConsoleColor.Yellow);
+            Console.WriteLine("Third Thread finished");
+        }
+    }
+}
